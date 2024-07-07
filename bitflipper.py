c44a90af7f7138da1040de60816d1e6b3070a1d7
bitflipperdev <bitflipper@certifiedsecure.com>
2024-07-07 21:54:16 +0200

    Create bitflipper.py

---
+++ bitflipper.py
@@ -0,0 +1,216 @@
+import asyncio
+import socket
+import argparse
+import logging
+import subprocess
+import datetime
+import re
+import time
+import signal
+import os
+import sys
+
+logger = logging.getLogger(__name__)
+
+STOP=False
+
+def signal_handler(sig, frame):
+    print('You pressed Ctrl+C!')
+    STOP=True
+
+
+class HTTPProtocol(asyncio.Protocol):
+    def __init__(self, message, on_con_lost, index):
+        self.message = f"GET /{message} HTTP/1.1\r\nHost: {HOSTNAME}\r\n\r\n"
+        self.on_con_lost = on_con_lost
+        self.count = 0
+        self.received = b""
+        self.index = index
+
+    def connection_made(self, transport):
+        transport.write(self.message.encode())
+        self.transport = transport
+        logging.debug(f"{self.index=} Data sent: {self.message!r}")
+
+    def data_received(self, data):
+        asyncio.ensure_future(self.process_data(data))
+
+    async def process_data(self, data):
+        if STOP:
+            self.transport.close()
+            return
+
+        self.received = self.received + data
+        if b"\r\n\r\n" in self.received:
+            logging.debug(f"{self.index=} Data received {self.count=} {self.received!r}")
+            self.received = b""
+            self.count = self.count + 1
+            if self.count == N :
+                self.transport.close()
+                return
+
+            await asyncio.sleep(W)
+            self.transport.write(self.message.encode())
+
+    def connection_lost(self, exc):
+        logging.debug(f"{self.index=} connection closed")
+        self.on_con_lost.set_result(True)
+
+
+async def run(loop, index):
+    if index % 10 == 0:
+        logging.info(f"Starting connection {index}")
+    else:
+        logging.debug(f"Starting connection {index}")
+    on_con_lost = loop.create_future()
+    message = 'A' * L
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(('0.0.0.0', 40000 + (index % 20000)))
+    sock.connect((IP, 80))
+
+    transport, protocol = await loop.create_connection(
+        lambda: HTTPProtocol(message, on_con_lost, index),
+        sock=sock)
+    try:
+        await asyncio.wait_for(on_con_lost, timeout=(N*W + 5))
+    finally:
+        transport.close()
+
+async def main(loop):
+    for x in range(int(C / P) + 1):
+        if STOP:
+            break
+        tasks = [run(loop, x*P+i) for i in range(min(P, C - x*P))]
+        try:
+            result = await asyncio.gather(*tasks, return_exceptions=IGNORE_ERRORS)
+        except Exception as e:
+            logging.critical(e, exc_info=True)
+            return
+        logging.debug(result)
+
+def verify_http():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((IP, 80))
+    message = 'A' * L
+    request = f"GET /{message} HTTP/1.1\r\nHost: {HOSTNAME}\r\n\r\n".encode()
+    logging.debug(f"verify_http sending {request=}")
+    sock.send(request)
+    response = b""
+    while not b"\r\n\r\n" in response:
+        response = response + sock.recv(4096)
+    logging.debug(f"verify_http received {response=}")
+    assert message.encode() in response, "This script expects the AAAA to be returned in the HTTP response"
+    assert not b"@" in response, "This script expects no @ in the HTTP response"
+    assert b"\r\nConnection: keep-alive\r\n" in response, "This script expects the connection to be kept alive"
+    sock.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+                        prog='Bitflipper',
+                        description='Check your connection for bitflips in packets')
+
+    parser.add_argument('-C', '--connections', default=400, type=int, help="Total number of TCP connections to use, with unique source ports (1-10000)")     
+    parser.add_argument('-P', '--parallel', default=5, type=int, help="Number of parallel TCP connections to use, this does not effect the total number of TCP connections to use (1-25)")
+    parser.add_argument('-N', '--num-requests', default=150, type=int, help="Number of HTTP requests to send per TCP connection (1-1000)")
+    parser.add_argument('-L', '--length', default=3500, type=int, help="Number of A's to use in the request")
+    parser.add_argument('-W', '--wait', default=0.03, type=float, help="Seconds to sleep after each HTTP request (float)")
+    parser.add_argument('-v', '--verbose', help="enable debugging", action='store_true', required=False, default=False)
+    parser.add_argument('-i', '--ignore-errors', help="do not stop when a connection error occurs (most likely due to bitflips)", action='store_true', required=False, default=False)
+    parser.add_argument('-q', '--quiet', help="do not show live tshark bitflips", action='store_true', required=False, default=False)
+    parser.add_argument('ip', help="IP-address to send HTTP requests to")
+    parser.add_argument('hostname', help="Hostname to put in the HTTP request-body")
+
+    args = parser.parse_args()
+
+    C = args.connections
+    P = args.parallel
+    N = args.num_requests
+    L = args.length
+    W = args.wait
+    IP = args.ip
+    QUIET = args.quiet
+    IGNORE_ERRORS = args.ignore_errors
+    HOSTNAME = args.hostname
+    WHEN = datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
+    PCAP=f"bitflipper_{IP}_{HOSTNAME}_{WHEN}.pcap"
+    REPORT=f"bitflipper_{IP}_{HOSTNAME}_{WHEN}.txt"
+
+    assert C >= 1 and C <= 10000, "Connections must be between 1 and 10000"
+    assert P >= 1 and P <= 25, "Parallel connections must be between 1 and 25"
+    assert N >= 1 and N <= 1000, "Number of HTTP requests per connection must be between 1 and 1000"
+    assert L >= 1 and L <= 10000, "Number of A's must be between 1 and 10000"
+
+    output = subprocess.check_output(["ip", "-4", "route", "show", "default"]).decode()
+    OUTGOING_INTERFACE=re.search("dev ([^ ]+)", output).group(1)
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+    logging.info(f"Testing with {C=} {P=} {N=} {L=} {W=} {IP=} {HOSTNAME=} {PCAP=} {REPORT=} {OUTGOING_INTERFACE=} {IGNORE_ERRORS=} {QUIET=}")
+
+    verify_http()
+
+
+    process_pcap = subprocess.Popen(["tcpdump", "-i", OUTGOING_INTERFACE, "-s", "0", "-w", PCAP, "-U", f"host {IP} and port 80"])
+    time.sleep(1)
+
+    if not QUIET:
+        tshark_pcap = subprocess.Popen(f"tail -c +1 -f {PCAP} | tshark -n -x -r - -Y 'tcp.payload contains \"@\"' -x | grep '@' | grep -v '^00'", shell=True, start_new_session=True)
+        time.sleep(1)
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(loop))
+
+    time.sleep(1)
+    process_pcap.terminate()
+    time.sleep(2)
+    if process_pcap.poll() is None:
+        process_pcap.kill()
+    if not QUIET:
+        pgrp = os.getpgid(tshark_pcap.pid)
+        os.killpg(pgrp, signal.SIGINT)
+        time.sleep(2)
+        tshark_pcap.wait()
+
+    print("Generating statistics\n\n")
+
+    SP_TOTAL=set()
+    SP_BF=set()
+    SP_BF_F=0
+    RX_TOTAL=0
+    RX_BF=0
+    RX_BF_F=0
+    RX_SP_BF=0
+    TX_TOTAL=0
+    p = subprocess.run(["tshark", "-n", "-r", PCAP, "-Y", f"ip.src == {IP} and tcp.srcport == 80 and tcp.payload contains \"A\"", "-T", "fields", "-e", "tcp.stream", "-e", "tcp.dstport", "-e", "frame"], capture_output=True, encoding="utf8")
+    for line in p.stdout.splitlines():
+        RX_TOTAL = RX_TOTAL + 1
+    p = subprocess.run(["tshark", "-n", "-r", PCAP, "-Y", f"ip.src == {IP} and tcp.srcport == 80 and tcp.payload contains \"@\"", "-T", "fields", "-e", "tcp.stream", "-e", "tcp.dstport", "-e", "frame"], capture_output=True, encoding="utf8")
+    for line in p.stdout.splitlines():
+        stream, port, _ = line.split("\t", 2)
+        SP_BF.add(port)
+        RX_BF = RX_BF + 1
+    SP_BF = len(SP_BF)
+    p = subprocess.run(["tshark", "-n", "-r", PCAP, "-Y", f"ip.dst == {IP} and tcp.dstport == 80 and tcp.payload contains \"A\"", "-T", "fields", "-e", "tcp.stream", "-e", "tcp.srcport", "-e", "frame"], capture_output=True, encoding="utf8")
+    for line in p.stdout.splitlines():
+        stream, port, _ = line.split("\t", 2)
+        SP_TOTAL.add(port)
+        TX_TOTAL=TX_TOTAL + 1
+    SP_TOTAL = len(SP_TOTAL)
+
+    print(f"""
+Report for {IP}/{HOSTNAME} at {WHEN}
+
+PCAP in {PCAP}
+
+OK packets: TX {TX_TOTAL}, RX {RX_TOTAL}
+Bitflip packets: {RX_BF} total, {((RX_BF/(RX_TOTAL+RX_BF))*100):.3f} % of all packets
+
+Total connections: {SP_TOTAL}
+Bitflip connections: {SP_BF} total, {((SP_BF / SP_TOTAL) * 100):.3f} % of all connections
+""")
+
